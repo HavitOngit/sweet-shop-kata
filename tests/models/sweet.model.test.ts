649a4a65a980e55d51131630d420263d444af585
HavitOngit <havitonline@gmail.com>
2025-07-17 14:23:05 +0530

    test: add model creation test

---
+++ tests/models/sweet.model.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from "vitest";
+
+describe("Sweet Model", () => {
+  describe("create", () => {
+    it("should create a valid sweet", () => {
+      const sweet = new SweetModel();
+    });
+  });
+});

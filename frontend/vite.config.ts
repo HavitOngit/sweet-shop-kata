a332a86a0f713287901aacb9f31d54cefbc5a87d
HavitOngit <havitonline@gmail.com>
2025-07-18 03:54:11 +0530

    chore: frontend setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), tailwindcss()],
   resolve: {
     alias: {
       "@core": resolve(__dirname, "../"),
+      "@": resolve(__dirname, "./src"),
     },
   },
 });

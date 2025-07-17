831c9714e9ed227e414f5cb76377e20c6103574c
HavitOngit <havitonline@gmail.com>
2025-07-17 20:27:01 +0530

    feat: implement createSweetShop function
    -to use in frontend
    -easy to access

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { SweetRepository } from "./src/repositories/sweet.repository";
+import {
+  SweetService,
+  type ISweetService,
+} from "./src/services/sweets.service";
+
+export function createSweetShop(): ISweetService {
+  const repository = new SweetRepository();
+  return new SweetService(repository);
+}

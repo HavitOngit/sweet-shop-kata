450d9d9f413412b2d7d6001c29401ec2e7408477
HavitOngit <havitonline@gmail.com>
2025-07-18 07:06:46 +0530

    feat: map can be passed from from outside
    - add reactivity of Map in svelte by using SvelteMap
    - ui styles

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,47 +1,37 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { createSweetShop } from "@core/";
+  import type { Sweet, SweetModel } from "@core/src/models/sweet.model";
+  import { SvelteMap } from "svelte/reactivity";
+
+  const sweetShop = $state(
+    createSweetShop(new SvelteMap<string, SweetModel>())
+  );
+
+  const sweets = $derived(sweetShop.getAllSweets());
+
+  const sweet: Sweet = {
+    id: "1",
+    name: "Chocolate Bar",
+    category: "Chocolate",
+    price: 20,
+    quantity: 10,
+  };
+
+  console.log("Sweet Shop Service Created:", sweetShop, typeof sweetShop);
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+{#if sweets.length === 0}
+  <p>No sweets available.</p>
+{:else}
+  <h1>Sweets List</h1>
+  {#each sweets as sweet}
+    <div>
+      <h2>{sweet.name}</h2>
+      <p>Category: {sweet.category}</p>
+      <p>Price: {sweet.price}</p>
+      <p>Quantity: {sweet.quantity}</p>
+    </div>
+  {/each}
+{/if}
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<button onclick={() => sweetShop.addSweet(sweet)}>Add Sweet</button>

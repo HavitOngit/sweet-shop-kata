1bcb54909ebe20021efc172eea416f95f7328421
HavitOngit <havitonline@gmail.com>
2025-07-18 13:22:42 +0530

    chore: removing old frontend

--- frontend/src/lib/store.svelte.ts
+++
@@ -1,7 +0,0 @@
-import { createSweetShop } from "@core/";
-import type { SweetModel } from "@core/src/models/sweet.model";
-import { SvelteMap } from "svelte/reactivity";
-
-export const sweetShop = $state(
-  createSweetShop(new SvelteMap<string, SweetModel>()),
-);
